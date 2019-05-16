48e9585553ba5656ac564b736ebeb546ab1eff57
Savannah C <sgcorrell@gmail.com>
2019-05-16 02:52:26 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,6 +41,6 @@ def select_character_names_and_number_of_books_they_are_in
    FROM characters, character_books
    WHERE character_books.character_id = characters.id 
 	 GROUP BY character_books.character_id
-	 ORDER BY COUNT(character_books.character_id) 
-	 DESC, characters.name ASC;"
+	 ORDER BY COUNT(character_books.character_id) DESC, 
+	 characters.name ASC;"
 end
