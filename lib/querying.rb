447c2da0a7aa3b09c5d6885fc306fcbf4a341ad0
Savannah C <sgcorrell@gmail.com>
2019-05-16 02:52:18 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,5 +37,10 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT DISTINCT characters.name, COUNT(character_books.character_id)
+   FROM characters, character_books
+   WHERE character_books.character_id = characters.id 
+	 GROUP BY character_books.character_id
+	 ORDER BY COUNT(character_books.character_id) 
+	 DESC, characters.name ASC;"
 end
