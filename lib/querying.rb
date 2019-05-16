3aaebc1300dcb35c784df1c6aec3cb0f06356f4e
Savannah C <sgcorrell@gmail.com>
2019-05-16 02:43:54 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species
+  FROM characters
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
