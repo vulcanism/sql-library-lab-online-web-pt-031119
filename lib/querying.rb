e91f5b7828650fd5d5f9933b60295e27763b56a8
Savannah C <sgcorrell@gmail.com>
2019-05-16 02:51:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,9 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM
   series INNER JOIN books ON series.id = books.series_id
-  "
+  INNER JOIN character_books ON books.id = character_books.book_id
+  INNER JOIN characters ON character_books.character_id = characters.id
+  WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
