f1fcc2bc1375ec93279400641bc92f1f75cb85cd
Savannah C <sgcorrell@gmail.com>
2019-05-16 02:49:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,8 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
-  "
+  FROM
+  series INNER JOIN books ON series.id = books."
 end
 
 def select_character_names_and_number_of_books_they_are_in
