d83c6116accf9ecf16501a73f06bb65c775280a6
Savannah C <sgcorrell@gmail.com>
2019-05-16 02:31:46 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,25 +1,20 @@
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT
-
 );
 
 CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
-
 );
 
 CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
-
 );
 
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
-
 );
 
 CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
-
 );
