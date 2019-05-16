2e363403950025dbcb379bdad00d7a98f8ec4ea3
Savannah C <sgcorrell@gmail.com>
2019-05-16 02:40:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,7 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year
   FROM books
-  WHERE "
+  WHERE books.series_id = 1"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
